fca942a55578fb7f6ae7ab41ca970c39818afd77
Gloria <74020369+GloDalSa@users.noreply.github.com>
2020-11-18 14:03:00 +0100

    Add files via upload

--- Project2_Charles/p2q7_GDS.m
+++ Project2_Charles/p2q7_GDS.m
@@ -23,20 +23,14 @@ rho = 0.01;
 pred = zeros(length(tau_vals), m);
 figure;
 fig = gcf;
-n = 10; m = 100;
-w_val = zeros(n,length(tau_vals));
-b_val = zeros(1,length(tau_vals));
 for k = 1 : length(tau_vals)
-   [w_val(:,k), b_val(:,k)] = quantile_regression(x(1:m,1:n), y(1:m), tau_vals(k), rho)
-   plot(SKIP_LEN : 50 + SKIP_LEN-1, pred(k, 1:50), 'Displayname', string(tau_vals(k)), 'LineWidth', 2)
+    [w,b] = quantile_regression(x(1:50,41:50), y(1:50), tau_vals(k), rho);
+    plot(SKIP_LEN : 50 + SKIP_LEN-1, x(1:50,41:50)*w+b, 'Displayname', string(tau_vals(k)), 'LineWidth', 2)
+    hold on
 end
 
 %% VISUALIZE
 plot(SKIP_LEN : 50 + SKIP_LEN-1, y(1:50), 'Displayname', 'actual', 'LineWidth', 2, 'color', 'k')
-hold on
-plot(SKIP_LEN : 50 + SKIP_LEN-1, w_val(:,1)'*x(49:98,1:n)'+b_val(1), 'Displayname', 'prediction \tau = 0.1', 'LineWidth', 2)
-plot(SKIP_LEN : 50 + SKIP_LEN-1, w_val(:,2)'*x(49:98,1:n)'+b_val(2), 'Displayname', 'prediction \tau = 0.5', 'LineWidth', 2)
-plot(SKIP_LEN : 50 + SKIP_LEN-1, w_val(:,3)'*x(49:98,1:n)'+b_val(3), 'Displayname', 'prediction \tau = 0.9', 'LineWidth', 2)
 xlabel('t')
 ylabel('Electricity prices')
 hold off
